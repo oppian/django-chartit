3779bc50c3498b463dfa55bf4cf47423041e6a42
oppiansteve <steve@oppian.com>
2014-04-27 14:54:51 +0100

    solve issue formating datetime to json prevent getting TypeError datetime.datime is not JSON serializable

--- chartit/templatetags/chartit.py
+++ chartit/templatetags/chartit.py
@@ -21,6 +21,9 @@ except AttributeError:
                                       CHARTIT_JS_REL_PATH,
                                       'chartloader.js')
 
+def date_format(obj):
+     return obj.isoformat() if hasattr(obj, 'isoformat') else obj
+
 register = template.Library()
 
 @register.filter
@@ -67,7 +70,8 @@ def load_charts(chart_list=None, render_to=''):
             if render_to:
                 hco['chart']['renderTo'] = render_to
         embed_script = (embed_script % (simplejson.dumps(chart_list, 
-                                                         use_decimal=True),
+                                                         use_decimal=True,
+                                                         default=date_format),
                                         CHART_LOADER_URL))
     else:
         embed_script = embed_script %((), CHART_LOADER_URL)
